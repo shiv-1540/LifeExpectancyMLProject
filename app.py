d46294a1732b94f8d54883f78e4e035480204d73
Shiv Ghyar <136742691+shiv-1540@users.noreply.github.com>
2024-11-23 20:51:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -119,5 +119,5 @@ def result():
     prediction = request.args.get('prediction')
     return render_template('result.html', prediction=prediction)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
